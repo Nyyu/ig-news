55de0d2b8ae2cc63bc6abafcc0f0defe0d5485ea
Nyyu <89911360+Nyyu@users.noreply.github.com>
2022-07-05 10:43:23 -0300

    some files

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,10 @@
+import { FaGithub } from 'react-icons/fa'
+
+export default function SignInButton() {
+    return (
+        <button type="button">
+            <FaGithub />
+            Sign in with Github
+        </button>
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from 'next/app'
+import Head from "next/head"
+
+import Header from "../components/Header"
+
+import '../styles/global.scss'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Head>
+      <title>ig.news</title>
+    </Head>
+    <Header />
+    <Component {...pageProps} />
+  </>
+}
+
+export default MyApp
